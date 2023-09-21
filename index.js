ed557eae206256926b66430dab016fac287eaa68
Usman Qidwai <usmanqidwai21@gmail.com>
2023-09-20 22:04:17 -0400

    Completed

---
+++ index.js
@@ -0,0 +1,21 @@
+function receivesAFunction(callback) {
+ 
+    callback();
+  }
+
+  function returnsANamedFunction() {
+ 
+    function namedFunction() {
+      console.log("Hello from the named function!");
+    }
+  
+  
+    return namedFunction;
+  }
+  function returnsAnAnonymousFunction(){
+    return function (){
+        console.log('Hello from an anonymous function!')
+    }
+   
+  }
+
